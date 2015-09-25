24965bfc9ebe4b070d50216efb175c4fd2244036
Bobby Ren <bobbyren@gmail.com>
2015-09-24 23:33:21 -0400

    added ConnectViewController

---
+++ WeTrainers/WeTrainers/ConnectViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ConnectViewController.swift
+//  WeTrainers
+//
+//  Created by Bobby Ren on 9/24/15.
+//  Copyright © 2015 Bobby Ren. All rights reserved.
+//
+
+import UIKit
+
+class ConnectViewController: UIViewController {
+
+    @IBOutlet var labelStatus: UILabel!
+    @IBOutlet var buttonAction: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func didClickButton(sender: UIButton) {
+    
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
