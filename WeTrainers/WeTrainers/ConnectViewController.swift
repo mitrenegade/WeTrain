32047288fc587acdb8894e46612a7f37b58d397e
Bobby Ren <bobbyren@gmail.com>
2015-09-25 00:59:52 -0400

    trainer status can toggle between available and off; connecting and training

--- WeTrainers/WeTrainers/ConnectViewController.swift
+++ WeTrainers/WeTrainers/ConnectViewController.swift
@@ -14,6 +14,8 @@ class ConnectViewController: UIViewController {
     @IBOutlet var labelStatus: UILabel!
     @IBOutlet var buttonAction: UIButton!
     
+    var status: String?
+    
     var shouldWarnIfRegisterPushFails: Bool = false // if user has denied push before, then registerForRemoteNotifications will not trigger a failure. Thus we have to manually warn after a certain time that the user needs to go to settings.
     
     override func viewDidLoad() {
@@ -21,15 +23,16 @@ class ConnectViewController: UIViewController {
 
         // Do any additional setup after loading the view.
         if !UIApplication.sharedApplication().isRegisteredForRemoteNotifications() {
+            self.status = "disconnected"
             self.labelStatus.text = "Notifications are not enabled"
             self.buttonAction.setTitle("Enable Notifications", forState: .Normal)
             NSNotificationCenter.defaultCenter().addObserver(self, selector: "pushEnabled", name: "push:enabled", object: nil)
         }
         else {
-            self.updateStatus()
+            self.refreshStatus()
         }
         
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: "warnForRemoteNotifications", name: "push:enable:failed", object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "warnForRemoteNotificationRegistrationFailure", name: "push:enable:failed", object: nil)
     }
     
     override func viewDidAppear(animated: Bool) {
@@ -43,28 +46,64 @@ class ConnectViewController: UIViewController {
     }
     
     @IBAction func didClickButton(sender: UIButton) {
+        let user = PFUser.currentUser()!
+        let trainer = user.objectForKey("trainer") as! PFObject
+        self.status = trainer.objectForKey("status") as? String
+
+        if self.status == "disconnected" {
+            self.registerForRemoteNotifications()
+        }
+        else if self.status == "off" || self.status == nil {
+            // start a shift
+            trainer.setObject("available", forKey: "status")
+            trainer.saveInBackgroundWithBlock({ (success, error) -> Void in
+                self.refreshStatus()
+            })
+        }
+        else if self.status == "available" {
+            // end a shift
+            trainer.setObject("off", forKey: "status")
+            trainer.saveInBackgroundWithBlock({ (success, error) -> Void in
+                self.refreshStatus()
+            })
+        }
+        else if self.status == "connecting" {
+            trainer.setObject("training", forKey: "status")
+            trainer.saveInBackgroundWithBlock({ (success, error) -> Void in
+                self.refreshStatus()
+            })
+        }
+        else if self.status == "training" {
+            trainer.setObject("available", forKey: "status")
+            trainer.saveInBackgroundWithBlock({ (success, error) -> Void in
+                self.refreshStatus()
+            })
+        }
+    }
+
+    func registerForRemoteNotifications() {
         let alert = UIAlertController(title: "Enable push notifications?", message: "To receive client notifications you must enable push. In the next popup, please click Yes.", preferredStyle: .Alert)
         alert.addAction(UIAlertAction(title: "Cancel", style: .Cancel, handler: nil))
         alert.addAction(UIAlertAction(title: "OK", style: .Default, handler: { (action) -> Void in
             let settings = UIUserNotificationSettings(forTypes: [.Alert, .Badge, .Sound], categories: nil)
             UIApplication.sharedApplication().registerUserNotificationSettings(settings)
             UIApplication.sharedApplication().registerForRemoteNotifications()
-
+            
             self.shouldWarnIfRegisterPushFails = true
             self.buttonAction.enabled = false
             self.buttonAction.alpha = 0.5
             let time = dispatch_time(DISPATCH_TIME_NOW, Int64(5 * Double(NSEC_PER_SEC)))
             dispatch_after(time, dispatch_get_main_queue()) { () -> Void in
                 if self.shouldWarnIfRegisterPushFails {
-                    self.warnForRemoteNotifications()
+                    self.warnForRemoteNotificationRegistrationFailure()
                 }
             }
-
+            
         }))
         self.presentViewController(alert, animated: true, completion: nil)
     }
-
-    func warnForRemoteNotifications() {
+    
+    func warnForRemoteNotificationRegistrationFailure() {
         self.shouldWarnIfRegisterPushFails = false
         self.buttonAction.enabled = true
         self.buttonAction.alpha = 1
@@ -82,12 +121,29 @@ class ConnectViewController: UIViewController {
         self.buttonAction.enabled = true
         self.buttonAction.alpha = 1
         self.shouldWarnIfRegisterPushFails = false
-        self.updateStatus()
+        self.refreshStatus()
     }
     
-    func updateStatus() {
-        self.labelStatus.text = "Waiting for client"
-        self.buttonAction.setTitle("Toggle my availability", forState: .Normal)
+    func refreshStatus() {
+        let user = PFUser.currentUser()!
+        let trainer = user.objectForKey("trainer") as! PFObject
+        let status: String? = trainer.objectForKey("status") as? String
+        if status == nil || status! == "off" {
+            self.labelStatus.text = "Off duty"
+            self.buttonAction.setTitle("Start shift", forState: .Normal)
+        }
+        else if status == "available" {
+            self.labelStatus.text = "Waiting for client"
+            self.buttonAction.setTitle("Go off duty", forState: .Normal)
+        }
+        else if status == "connecting" {
+            self.labelStatus.text = "A client is available"
+            self.buttonAction.setTitle("Accept training request", forState: .Normal)
+        }
+        else if status == "training" {
+            self.labelStatus.text = "Training session"
+            self.buttonAction.setTitle("Complete workout", forState: .Normal)
+        }
     }
     /*
     // MARK: - Navigation
