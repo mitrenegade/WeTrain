71fa571d4a8a8d98d59506592562e041fd702850
Bobby Ren <bobbyren@gmail.com>
2015-10-02 00:35:19 -0400

    added a passcode check and start button

--- WeTrainers/WeTrainers/ClientInfoViewController.swift
+++ WeTrainers/WeTrainers/ClientInfoViewController.swift
@@ -15,14 +15,25 @@ class ClientInfoViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var labelName: UILabel!
+    
+    @IBOutlet weak var viewInfo: UIView!
     @IBOutlet weak var labelInfo: UILabel!
+    @IBOutlet weak var constraintLabelInfoHeight: NSLayoutConstraint?
+    
+    @IBOutlet weak var labelPasscode: UILabel!
     @IBOutlet weak var inputPasscode: UITextField!
+    
+    @IBOutlet weak var labelStart: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.imageView.layer.borderWidth = 1
         self.imageView.layer.borderColor = UIColor.darkGrayColor().CGColor
         self.imageView.layer.cornerRadius = 5
+
+        self.viewInfo.layer.borderWidth = 1
+        self.viewInfo.layer.borderColor = UIColor.lightGrayColor().CGColor
+        self.viewInfo.layer.cornerRadius = 5
 
         // Do any additional setup after loading the view.
         request.fetchIfNeededInBackgroundWithBlock { (object, error) -> Void in
@@ -64,6 +75,10 @@ class ClientInfoViewController: UIViewController, UITextFieldDelegate {
                 self.labelInfo.text = info
             })
         }
+        
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Start", style: .Done, target: self, action: "startWorkout")
+        self.validatePasscode()
+        self.labelStart.hidden = true;
     }
     
     func ageOfClient(client: PFObject) -> String? {
@@ -83,7 +98,27 @@ class ClientInfoViewController: UIViewController, UITextFieldDelegate {
     
     func textFieldShouldReturn(textField: UITextField) -> Bool {
         self.inputPasscode.resignFirstResponder()
+        
+        self.validatePasscode()
+        
         return true
+    }
+    
+    func validatePasscode() {
+        let text = self.inputPasscode.text
+        
+        if text?.lowercaseString == "lift" {
+            self.navigationItem.rightBarButtonItem?.enabled = true
+        }
+        else {
+            self.navigationItem.rightBarButtonItem?.enabled = false
+        }
+    }
+    
+    func startWorkout() {
+        self.labelStart.hidden = false
+        self.labelPasscode.hidden = true
+        self.inputPasscode.hidden = true
     }
 
     /*
