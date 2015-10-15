2556476f3889e8b5b3288c5ac5c7c709d5398f74
Bobby Ren <bobbyren@gmail.com>
2015-10-15 17:02:55 -0400

    removed credit card requirement for request

--- WeTrain/MapViewController.swift
+++ WeTrain/MapViewController.swift
@@ -212,10 +212,13 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, GMSMapView
     @IBAction func didClickRequest(sender: UIButton) {
         let client: PFObject = PFUser.currentUser()!.objectForKey("client") as! PFObject
         let payment = client.objectForKey("stripeToken")
+        /*
+        // TODO: credit card is disabled for initial review. put this back in when stripe is legit
         if payment == nil {
             self.simpleAlert("Please enter payment", message: "You must enter a credit card before requesting a trainer. Go to the Account tab to update your payment.")
             return
         }
+        */
         
         if self.currentLocation != nil {
             let coder = GMSGeocoder()
