8a8c987e37ca6558dece5d736a7132288a354b30
Bobby Ren <bobbyren@gmail.com>
2015-11-30 01:36:49 -0500

    CLOUD: checks for customer_id before allowing client to request workout; CLIENT: requires customer_id to exist before requesting workout

--- clouddev/cloud/main.js
+++ clouddev/cloud/main.js
@@ -176,7 +176,6 @@ Parse.Cloud.beforeSave("Workout", function(request, response) {
         console.log("started training " + trainingObject.id + " at " + start)
     }
     console.log("beforeSave workout status " + trainingObject.get("status"))
-    /*
     // TODO: allow clients to request a workout and handle failure on trainer's side, until client's app is released
     if (trainingObject.get("status") == "requested") {
         var client = trainingObject.get("client")
@@ -192,8 +191,6 @@ Parse.Cloud.beforeSave("Workout", function(request, response) {
     else {
         response.success()
     } 
-    */
-        response.success()
 });
 
 Parse.Cloud.afterSave("Workout", function(request, response) {
